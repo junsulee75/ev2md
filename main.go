1151f67a308df40bc174e3cc8c3a092bb1405709
junsu.lee <junsu.lee@servicenow.com>
2026-04-26 17:20:01 +1000

    remove servicenow.com email
    this repo has zero codes related to servicenow

--- main.go
+++ main.go
@@ -151,7 +151,7 @@ func parseArgs() (inputFiles []string, outputFile string, cleanMode, cpMode, res
 }
 
 func printUsage() {
-	fmt.Printf("ev2md  (build: %s)  junsu.lee@servicenow.com\n", buildDate)
+	fmt.Printf("ev2md  (build: %s)\n", buildDate)
 	fmt.Println()
 	fmt.Println("Usage:")
 	fmt.Println("  ev2md <input.enex> [-o output_dir] [-r]")
